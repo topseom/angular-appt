539024ed7d1b5ed6976507ef4117ded0b00a8535
Monchai <monchai.j@seven.co.th>
2017-12-25 09:47:46 +0700

    add module slide

--- src/modules/ecate/ecate.module.ts
+++ src/modules/ecate/ecate.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { EcateFormService } from './providers/form'
 
+import { SwiperModule,SwiperConfigInterface,SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+
+export const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    keyboard: true
+};
+
 @NgModule({
+    imports: [
+        SwiperModule
+    ],
+    exports:[
+        SwiperModule
+    ],
     providers:[
-        EcateFormService
+        EcateFormService,
+        {
+            provide: SWIPER_CONFIG,
+            useValue: DEFAULT_SWIPER_CONFIG
+        }
     ]
 })
 
--- src/modules/ecom/ecom.module.ts
+++ src/modules/ecom/ecom.module.ts
@@ -4,12 +4,30 @@ import { EcomCartService } from './providers/cart';
 import { EcomCouponService } from './providers/coupon';
 import { EcomWishlistService } from './providers/wishlist';
 
+import { SwiperModule,SwiperConfigInterface,SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+
+export const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    keyboard: true
+};
+
 @NgModule({
+    imports: [
+        SwiperModule
+    ],
+    exports:[
+        SwiperModule
+    ],
     providers:[
         AuthService,
         EcomCouponService,
         EcomCartService,
-        EcomWishlistService
+        EcomWishlistService,
+        {
+            provide: SWIPER_CONFIG,
+            useValue: DEFAULT_SWIPER_CONFIG
+        }
     ]
 })
 
